8989d7272558af9608cbef476d8a19589840e88a
StiveMan1 <39464416+StiveMan1@users.noreply.github.com>
2021-05-29 17:55:25 +0300

    Updated for new version of dart

--- lib/src/RsaKey.dart
+++ lib/src/RsaKey.dart
@@ -1,7 +1,6 @@
 import 'Primality.dart';
 
 import 'dart:convert';
-import 'dart:math';
 import 'dart:typed_data';
 import "package:asn1lib/asn1lib.dart";
 // import 'dart:convert';
@@ -13,8 +12,8 @@ import 'package:basic_utils/basic_utils.dart';
 
 class RsaKey{
     static BigInt E = BigInt.parse("2348732984757349847638174698237628374682364862834421");
-    BigInt _n,_e,_p,_q,_d=null,_u,_dp,_dq;
-    RsaKey({String password = null ,BigInt n = null,BigInt e = null,BigInt p = null,BigInt q = null,BigInt d = null,BigInt u = null,}){
+    BigInt? _n,_e,_p,_q,_d,_u,_dp,_dq;
+    RsaKey({String? password,BigInt? n,BigInt? e,BigInt? p,BigInt? q,BigInt? d,BigInt? u,}){
       if(n != null && e != null){
         this._n = n;
         this._e = e;
@@ -23,98 +22,88 @@ class RsaKey{
           this._p = p;
           this._d = d;
           this._u = u;
-          this._dp = this._d % (this._p - BigInt.one);  // = (e⁻¹) mod (p-1)
-          this._dq = this._d % (this._q - BigInt.one);  // = (e⁻¹) mod (q-1)
+          this._dp = this._d! % (this._p! - BigInt.one);  // = (e⁻¹) mod (p-1)
+          this._dq = this._d! % (this._q! - BigInt.one);  // = (e⁻¹) mod (q-1)
         }
       }
     }
-    BigInt n(){
+    BigInt? n(){
       return this._n;
     }
-    BigInt e(){
+    BigInt? e(){
       return this._e;
     }
-    BigInt p(){
-      if (!has_private()){
+    BigInt? p(){
+      if (!hasPrivate()){
         throw("No private exponent available for public keys");
       }
       return this._p;
     }
-    BigInt q(){
-      if (!has_private()){
+    BigInt? q(){
+      if (!hasPrivate()){
         throw("No private exponent available for public keys");
       }
       return this._q;
     }
-    BigInt d(){
-      if (!has_private()){
+    BigInt? d(){
+      if (!hasPrivate()){
         throw("No private exponent available for public keys");
       }
       return this._d;
     }
-    BigInt u(){
-      if (!has_private()){
+    BigInt? u(){
+      if (!hasPrivate()){
         throw("No private exponent available for public keys");
       }
       return this._u;
-    }
-
-
-    int size_in_bits_(){
-      // """Size of the RSA modulus in bits"""
-      return size_in_bits(_n);
-    }
-    int size_in_bytes_(){
-      // """The minimal amount of bytes that can hold the RSA modulus"""
-      return (size_in_bits(_n) - 1) ~/ 8 + 1;
     }
 
     BigInt _encrypt( plaintext){
         BigInt res = BigInt.zero;
         BigInt plain = BigInt.parse(plaintext.toString());
         while(plain > BigInt.zero){
-            res*=_n;
-            res+=binpow(plain%_n, _e, _n);
-            plain~/=_n;
+            res*=_n!;
+            res+=binpow(plain%_n!, _e!, _n!);
+            plain~/=_n!;
         }
         return res;
     }
     BigInt _decrypt( plaintext){
-        if (!has_private()){
+        if (!hasPrivate()){
           throw("This is not a private key");
         }
         BigInt res = BigInt.zero;
         BigInt text = BigInt.parse(plaintext.toString());
         while (text > BigInt.zero){
-            BigInt ciphertext = text%this._n;
-            text~/=this._n;
-            res*=this._n;
+            BigInt ciphertext = text%this._n!;
+            text~/=this._n!;
+            res*=this._n!;
             // Blinded RSA decryption (to prevent timing attacks):
             // Step 1: Generate random secret blinding factor r,
             // such that 0 < r < n-1
-            BigInt r = My_Random('').random_range(min_inclusive:BigInt.one, max_exclusive:this._n);
+            BigInt r = MyRandom('').randomRange(minInclusive:BigInt.one, maxExclusive:this._n);
             // Step 2: Compute c' = c * r**e mod n
-            BigInt cp = ciphertext * binpow(r, this._e, this._n) % this._n;
+            BigInt cp = ciphertext * binpow(r, this._e!, this._n!) % this._n!;
             // Step 3: Compute m' = c'**d mod n       (normal RSA decryption)
-            BigInt m1 = binpow(cp, this._dp, this._p);
-            BigInt m2 = binpow(cp, this._dq, this._q);
-            BigInt h = ((m2 - m1) * this._u) % this._q;
-            BigInt mp = h * this._p + m1;
+            BigInt m1 = binpow(cp, this._dp!, this._p!);
+            BigInt m2 = binpow(cp, this._dq!, this._q!);
+            BigInt h = ((m2 - m1) * this._u!) % this._q!;
+            BigInt mp = h * this._p! + m1;
             // Step 4: Compute m = m**(r-1) mod n
-            res+= (inverse(r, this._n) * mp) % this._n;
+            res+= (inverse(r, this._n!) * mp) % this._n!;
         }
         // Verify no faults occurred
         return res;
     }
 
-    bool has_private(){
+    bool hasPrivate(){
         // """Whether this is an RSA private key"""
         return (this._d != null);
     }
-    bool can_encrypt(){  // legacy
+    bool canEncrypt(){  // legacy
         return true;
     }
-    bool can_sign(){     // legacy
+    bool canSign(){     // legacy
         return true;
     }
 
@@ -122,46 +111,43 @@ class RsaKey{
       return RsaKey(n:_n, e:_e);
     }
 
-    String exportKey({format='PEM', passphrase=null, pkcs=1, protection=null, randfunc=null}){
+    String exportKey({format='PEM', passphrase, pkcs=1, protection, randfunc}){
 
         if(randfunc == null){
-            randfunc = My_Random('LOLLSKASKL');
+            randfunc = MyRandom('LOLLSKASKL');
         }
 
         if(format == 'OpenSSH'){
-          List<int> str_bytes = str_to_bytes('ssh-rsa');
-          List<int> n_bytes = to_bytes(_n);
-          List<int> e_bytes = to_bytes(_e);
+          List<int> strBytes = str2Bytes('ssh-rsa');
+          List<int> nBytes = toBytes(_n!);
+          List<int> eBytes = toBytes(_e!);
 
-          if (n_bytes[0] & 0x80 == 0x80){
-            n_bytes = [0] + n_bytes;
+          if (nBytes[0] & 0x80 == 0x80){
+            nBytes = [0] + nBytes;
           }
-          if (e_bytes[0] & 0x80 == 0x80){
-            e_bytes = [0] + e_bytes;
+          if (eBytes[0] & 0x80 == 0x80){
+            eBytes = [0] + eBytes;
           }
 
-          str_bytes = pack_len(str_bytes.length) + str_bytes;
-          n_bytes = pack_len(n_bytes.length) + n_bytes;
-          e_bytes = pack_len(e_bytes.length) + e_bytes;
-          return 'ssh-rsa ' + base64.encode(str_bytes + n_bytes + e_bytes);
+          strBytes = packLength(strBytes.length) + strBytes;
+          nBytes = packLength(nBytes.length) + nBytes;
+          eBytes = packLength(eBytes.length) + eBytes;
+          return 'ssh-rsa ' + base64.encode(strBytes + nBytes + eBytes);
         }
         if(format == 'Sakaar'){
-          List<int> n_bytes = to_bytes(_n);
+          List<int> nBytes = toBytes(_n!);
 
-          if (n_bytes[0] & 0x80 == 0x80){
-            n_bytes = [0] + n_bytes;
+          if (nBytes[0] & 0x80 == 0x80){
+            nBytes = [0] + nBytes;
           }
-          n_bytes = pack_len(n_bytes.length) + n_bytes;
-          return base64.encode(n_bytes);
+          nBytes = packLength(nBytes.length) + nBytes;
+          return base64.encode(nBytes);
         }
-        String key_type = '';
-        if(has_private()){
+        if(hasPrivate()){
           return RSAPKCSParser.encodePrivateKeyToPem(this);
         }else{
           return RSAPKCSParser.encodePublicKeyToPem(this);
         }
-
-        throw("Unknown key format '" + format.toString() + "'. Cannot export the RSA key.");
     }
 
 
@@ -196,7 +182,7 @@ class RsaKey{
     size(M, B){
         throw 'NotImplementedError';
     }
-  static RsaKey importKey(String extern_key, {passphrase=null}){
+  static RsaKey importKey(String externKey, {passphrase}){
 
     // from CryptoCore.IO import PEM;
 
@@ -205,42 +191,42 @@ class RsaKey{
     //     passphrase = tobytes(passphrase);
     // }
 
-    if (extern_key.startsWith('-----BEGIN OPENSSH PRIVATE KEY')){
+    if (externKey.startsWith('-----BEGIN OPENSSH PRIVATE KEY')){
         // String text_encoded = extern_key.toString();
         // openssh_encoded, marker, enc_flag = PEM.decode(text_encoded, passphrase);
         // result = _import_openssh_private_rsa(openssh_encoded, passphrase);
         // return result;
     }
 
-    if (extern_key.startsWith('-----')){
-        return RSAPKCSParser().parsePEM(extern_key);
+    if (externKey.startsWith('-----')){
+        return RSAPKCSParser().parsePem(externKey)!;
     }
 
-    if(extern_key.startsWith('ssh-rsa ')){
-        String lol = extern_key.split(' ')[1];
+    if(externKey.startsWith('ssh-rsa ')){
+        String lol = externKey.split(' ')[1];
         List<int> list = base64.decode(lol);
 
         
-        var len = unpack_len(list.sublist(0,4));
+        var len = unpackLength(list.sublist(0,4));
         list = list.sublist(len+4);
 
         // Get n
-        len = unpack_len(list.sublist(0,4));
-        BigInt n = from_bytes(list.sublist(4,len+4));
+        len = unpackLength(list.sublist(0,4));
+        BigInt n = fromBytes(list.sublist(4,len+4));
         list = list.sublist(len+4);
 
         // Get e
-        len = unpack_len(list.sublist(0,4));
-        BigInt e = from_bytes(list.sublist(4,len+4));
+        len = unpackLength(list.sublist(0,4));
+        BigInt e = fromBytes(list.sublist(4,len+4));
         list = list.sublist(len+4);
 
         return RsaKey(n: n , e: e);
     }
-    if(extern_key.startsWith('Sakaar: ')){
-        String lol = extern_key.split(' ')[1];
+    if(externKey.startsWith('Sakaar: ')){
+        String lol = externKey.split(' ')[1];
         List<int> list = base64.decode(lol);
-        var len = unpack_len(list.sublist(0,4));
-        BigInt n = from_bytes(list.sublist(4,len+4));
+        var len = unpackLength(list.sublist(0,4));
+        BigInt n = fromBytes(list.sublist(4,len+4));
         list = list.sublist(len+4);
 
         return RsaKey(n: n , e: RsaKey.E);
@@ -250,49 +236,47 @@ class RsaKey{
 
   }
   // Genaratink private key from random function or password
-  static generate(bits, {My_Random randfunc = null, String password = null}){
+  static generate(bits, {MyRandom? randfunc, String? password}){
     if(password == null && randfunc == null){
       throw("randfunc and password can not be both None");
     }
     if(password != null){
-      randfunc = My_Random(password);
+      randfunc = MyRandom(password);
     }
     BigInt e = BigInt.parse('2348732984757349847638174698237628374682364862834421');
     BigInt d = BigInt.from(1);
     BigInt n = d;
-    BigInt p;
-    BigInt q;
-    BigInt lcm;
-    int size_q;
-    int size_p;
-    BigInt min_q;
-    BigInt min_p;
-    BigInt min_distance;
-    while(size_in_bits(n) != bits && d < (BigInt.one << (bits ~/ 2))){
-        size_q = bits ~/ 2;
-        size_p = bits - size_q;
+    BigInt? p;
+    BigInt? q;
+    int? sizeQ;
+    int? sizeP;
+    BigInt? minQ;
+    BigInt? minP;
+    BigInt? minDistance;
+    while(sizeInBits(n) != bits && d < (BigInt.one << (bits ~/ 2))){
+        sizeQ = bits ~/ 2;
+        sizeP = bits - sizeQ;
 
-        min_q = sqrt(BigInt.from(1) << (2 * size_q - 1));
-        min_p = min_q;
-        if (size_q != size_p){
-            min_p = sqrt(BigInt.from(1) << (2 * size_p - 1));
+        minQ = sqrt(BigInt.from(1) << (2 * sizeQ! - 1));
+        minP = minQ;
+        if (sizeQ != sizeP){
+            minP = sqrt(BigInt.from(1) << (2 * sizeP! - 1));
         }
 
-        p = generate_probable_prime(exact_bits:size_p,  randfunc:randfunc, prime_filter : (BigInt candidate){
-            return candidate > min_p && (candidate - BigInt.one).gcd(e) == BigInt.one;
+        p = generateProbablePrime(exactBits:sizeP,  randfunc:randfunc, primeFilter : (BigInt candidate){
+            return candidate > minP! && (candidate - BigInt.one).gcd(e) == BigInt.one;
         });
-        min_distance = BigInt.one << (bits ~/ 2 - 100);
+        minDistance = BigInt.one << (bits ~/ 2 - 100);
 
-        q = generate_probable_prime(exact_bits:size_q,
-                                    randfunc:randfunc,prime_filter:(BigInt candidate){
-          return (candidate > min_q && (candidate - BigInt.one).gcd(e) == BigInt.one && abs(candidate - p) > min_distance);
+        q = generateProbablePrime(exactBits:sizeQ,
+                                    randfunc:randfunc,primeFilter:(BigInt candidate){
+          return (candidate > minQ! && (candidate - BigInt.one).gcd(e) == BigInt.one && abs(candidate - p!) > minDistance!);
         });
 
         n = p * q;
-        lcm = LCM(p - BigInt.one, q - BigInt.one);
-        d = inverse(e, lcm);
+        d = inverse(e, lcm(p - BigInt.one, q - BigInt.one));
     }
-    if (p > q){
+    if (p! > q!){
       BigInt x = q;
       q = p;
       p = x;
@@ -330,7 +314,7 @@ class RSAPKCSParser {
   static const String certFooter = '-----END CERTIFICATE-----';
 
   /// Parse PEM
-  RsaKey parsePEM(String pem, {String password}) {
+  RsaKey? parsePem(String pem, {String? password}) {
     final List<String> lines = pem
         .split('\n')
         .map((String line) => line.trim())
@@ -340,16 +324,17 @@ class RSAPKCSParser {
     if (lines.isEmpty) {
       _error('format error');
     }
-    RsaKey publicKey = _publicKey(lines);
-    RsaKey privateKey = _privateKey(lines);
+    RsaKey? publicKey = _publicKey(lines);
+    RsaKey? privateKey = _privateKey(lines);
     if(privateKey != null){
       return privateKey;
     }else if(publicKey != null){
       return publicKey;
     }
+    return null;
   }
 
-  RsaKey _privateKey(List<String> lines, {String password}) {
+  RsaKey? _privateKey(List<String> lines, {String? password}) {
     int header;
     int footer;
 
@@ -372,14 +357,14 @@ class RSAPKCSParser {
     final Uint8List keyBytes = Uint8List.fromList(base64.decode(key));
     final ASN1Parser p = ASN1Parser(keyBytes);
 
-    final ASN1Sequence seq = p.nextObject();
+    final ASN1Sequence seq = p.nextObject() as ASN1Sequence;
 
     if (lines[header] == pkcs1PrivateHeader) {
       return _pkcs1PrivateKey(seq);
     } else if (lines[header] == pkcs8PrivateHeader) {
       return _pkcs8PrivateKey(seq);
     } else {
-      return _pkcs8PrivateEncKey(seq, password);
+      return _pkcs8PrivateEncKey(seq, password!);
     }
   }
 
@@ -404,7 +389,7 @@ class RSAPKCSParser {
       p: asn1Ints[4].valueAsBigInteger,
       q: asn1Ints[5].valueAsBigInteger,
       e: asn1Ints[2].valueAsBigInteger,
-      u: inverse(asn1Ints[4].valueAsBigInteger, asn1Ints[5].valueAsBigInteger)
+      u: inverse(asn1Ints[4].valueAsBigInteger as BigInt, asn1Ints[5].valueAsBigInteger as BigInt)
     );
     // for(int i=0;i<9;i++){
     //   print(i);
@@ -414,12 +399,12 @@ class RSAPKCSParser {
   }
 
   RsaKey _pkcs8PrivateKey(ASN1Sequence seq) {
-    final ASN1OctetString os = seq.elements[2];
+    final ASN1OctetString os = seq.elements[2] as ASN1OctetString;
     final ASN1Parser p = ASN1Parser(os.valueBytes());
-    return _pkcs1PrivateKey(p.nextObject());
+    return _pkcs1PrivateKey(p.nextObject() as ASN1Sequence);
   }
 
-  RsaKey _publicKey(List<String> lines) {
+  RsaKey? _publicKey(List<String> lines) {
     int header;
     int footer;
     if (lines.contains(pkcs1PublicHeader)) {
@@ -441,7 +426,7 @@ class RSAPKCSParser {
     final Uint8List keyBytes = Uint8List.fromList(base64.decode(key));
     final ASN1Parser p = ASN1Parser(keyBytes);
 
-    final ASN1Sequence seq = p.nextObject();
+    final ASN1Sequence seq = p.nextObject() as ASN1Sequence;
 
     if (lines[header] == pkcs1PublicHeader) {
       return _pkcs1PublicKey(seq);
@@ -462,10 +447,10 @@ class RSAPKCSParser {
   }
 
   RsaKey _pkcs8PublicKey(ASN1Sequence seq) {
-    final ASN1BitString os = seq.elements[1]; //ASN1OctetString or ASN1BitString
+    final ASN1BitString os = seq.elements[1] as ASN1BitString; //ASN1OctetString or ASN1BitString
     final Uint8List bytes = os.valueBytes().sublist(1);
     final ASN1Parser p = ASN1Parser(bytes);
-    return _pkcs1PublicKey(p.nextObject());
+    return _pkcs1PublicKey(p.nextObject() as ASN1Sequence);
   }
 
   void _error(String msg) {
@@ -479,8 +464,8 @@ class RSAPKCSParser {
       algorithmSeq.add(paramsAsn1Obj);
 
       var publicKeySeq = new ASN1Sequence();
-      publicKeySeq.add(ASN1Integer(publicKey.n()));
-      publicKeySeq.add(ASN1Integer(publicKey.e()));
+      publicKeySeq.add(ASN1Integer(publicKey.n()!));
+      publicKeySeq.add(ASN1Integer(publicKey.e()!));
       var publicKeySeqBitString = new ASN1BitString(Uint8List.fromList(publicKeySeq.encodedBytes));
 
       var topLevelSeq = new ASN1Sequence();
@@ -503,14 +488,14 @@ class RSAPKCSParser {
       ASN1Sequence privateKeySeq = new ASN1Sequence();
 
       privateKeySeq.add(version);
-      privateKeySeq.add(ASN1Integer(privateKey._n));
-      privateKeySeq.add(ASN1Integer(privateKey._e));
-      privateKeySeq.add(ASN1Integer(privateKey._d));
-      privateKeySeq.add(ASN1Integer(privateKey._p));
-      privateKeySeq.add(ASN1Integer(privateKey._q));
-      privateKeySeq.add(ASN1Integer(privateKey._d % (privateKey._p - BigInt.from(1))));
-      privateKeySeq.add(ASN1Integer(privateKey._d % (privateKey._q - BigInt.from(1))));
-      privateKeySeq.add(ASN1Integer(inverse(privateKey._q, privateKey._p)));
+      privateKeySeq.add(ASN1Integer(privateKey._n!));
+      privateKeySeq.add(ASN1Integer(privateKey._e!));
+      privateKeySeq.add(ASN1Integer(privateKey._d!));
+      privateKeySeq.add(ASN1Integer(privateKey._p!));
+      privateKeySeq.add(ASN1Integer(privateKey._q!));
+      privateKeySeq.add(ASN1Integer(privateKey._d! % (privateKey._p! - BigInt.from(1))));
+      privateKeySeq.add(ASN1Integer(privateKey._d! % (privateKey._q! - BigInt.from(1))));
+      privateKeySeq.add(ASN1Integer(inverse(privateKey._q!, privateKey._p!)));
       
       var dataBase64 = base64.encode(privateKeySeq.encodedBytes);
       var chunks =( StringUtils.chunk(dataBase64, 64)).join('\n');
